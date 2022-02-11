a805f53b158cb2c60a267d32ff16c6a759cdf234
Spyder-Monkey <tbender@ptd.net>
2022-02-11 12:09:06 -0500

    make changes to database config to find ini file

--- database/config.py
+++ database/config.py
@@ -3,7 +3,7 @@ import os
 
 DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-def config_poll_db(filename=DIR+'\database\database.ini', section='mysql_poll'):
+def config_poll_db(filename=DIR+'/database/database.ini', section='mysql_poll'):
     # create a parser
     parser = ConfigParser()
     # Read config file
